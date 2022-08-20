c21e9b3f42272943059d628446c9fc467c901f0a
Emerson Millard <emillard.code@gmail.com>
2022-08-20 12:00:00 -0700

    Created list.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  products : any;
+
+  constructor() {
+    this.products = [
+      {
+        "id": 4387,
+        "name": "watch",
+        "brand": "Timex",
+        "price": 7000
+      }, {
+        "id": 3877,
+        "name": "Mobile",
+        "brand": "One Plus",
+        "price": 55000
+      }, {
+        "id": 8938,
+        "name": "Book",
+        "brand": "head first",
+        "price": 999
+      }, {
+        "id": 3780,
+        "name": "T-Shirt",
+        "brand": "US POLO",
+        "price": 1699
+      }, {
+        "id": 5899,
+        "name": "AC",
+        "brand": "LG",
+        "price": 30000
+      }
+    ]
+  }
 
   ngOnInit() {
   }
